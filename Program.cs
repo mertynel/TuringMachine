a4b566ded452e5b5f1a86beaeb44671c55cbd6d7
martynas.ulinskas <martynas.ulinskas@festo.com>
2018-09-24 12:51:12 +0300

    data reading fix

--- Program.cs
+++ Program.cs
@@ -10,35 +10,41 @@ namespace Turing_Machiene
     {
         static void Main(string[] args)
         {
-            string path = @"data.txt";
+            string path = @"C:\Git Repositories\TuringMachine\data.txt"; // pasikeisk kur yra tekstas
             int startPosition = 0; //set default starting position
             int iteration = 0;
-            string [] taskLine;
-            string [] data = new string[4];
+            string[] taskLine;
+            string[] data = new string[4];
             char[] line;
             var tasksList = new List<Task>();
 
-        using (FileStream fs = File.OpenRead(path)) // text reading copied from google
-        {
-            byte[] b = new byte[1024];
-            UTF8Encoding temp = new UTF8Encoding(true);
-            while (fs.Read(b,0,b.Length) > 0)
+            //********************************************************* Data reading ********************************************
+            using (FileStream fs = File.OpenRead(path)) // text reading copied from google
             {
-                data = temp.GetString(b).Split("\n");
-            }
-
-            startPosition = Int32.Parse(data[0]);
-                line = data[1].ToCharArray();
-                for(int i = 2; i < data.Length; i++)
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b, 0, b.Length) > 0)
                 {
-                    if(data[i].Length > 0 && data[i].Length < 10)
+                    data = temp.GetString(b).Split("\r\n"); // \n -- unix(Linux, and OSX) \r\n - windows
+                }
+
+                startPosition = Int32.Parse(data[0]);
+                line = data[1].ToCharArray();
+                for (int i = 2; i < data.Length; i++)
+                {
+                    if (data[i].Length > 0 && data[i].Length < 12)
                     {
                         taskLine = data[i].Split(" ");
-                        int _position = int.Parse(taskLine[0]);
-                        int _nextPosition = int.Parse(taskLine[4]);
+                        bool _positioIsNumber = int.TryParse(taskLine[0], out int _position);
+                        bool _nextPositionIsNumber = int.TryParse(taskLine[4], out int _nextPosition);
+
+                        if (!_nextPositionIsNumber)
+                        {
+                            _nextPosition = -1;
+                        }
                         var _task = tasksList.FirstOrDefault(x => x.position == _position);
 
-                        if(_task == null)
+                        if (_task == null)
                         {
                             var notExistingTask = new Task();
                             var taskAction = new Action();
@@ -52,8 +58,11 @@ namespace Turing_Machiene
                                 }
                             };
                             tasksList.Add(notExistingTask);
-                        } else {
-                            _task.tasks.Add(new Action {
+                        }
+                        else
+                        {
+                            _task.tasks.Add(new Action
+                            {
                                 currentState = taskLine[1],
                                 nextState = taskLine[2],
                                 moveDirection = taskLine[3],
@@ -62,8 +71,9 @@ namespace Turing_Machiene
                         }
                     }
                 }
-        }
-            Console.WriteLine("Hello World!");
+            }
+
+            //********************************************** Turing Machine **********************************************
         }
     }
 }
